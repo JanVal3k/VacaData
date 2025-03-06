788d72a9da18ddb8a17ea26c4a17e400cb65144e
HernanGomezM <dariog_36@hotmail.com>
2025-03-06 15:53:20 -0500

    feat: alineacion de los demas componetes dentro del espacio de trabajo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MenuComponent } from "./menu/menu.component";
+import { AnimalsComponent } from "./animals/animals.component";
+import { HealthComponent } from "./health/health.component";
+import { ReproducctionComponent } from "./reproducction/reproducction.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MenuComponent],
+  imports: [RouterOutlet, MenuComponent, AnimalsComponent, HealthComponent,ReproducctionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
