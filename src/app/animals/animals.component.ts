90f94f3dfd6283a8aadcb676ef103d0770e9091c
HernanGomezM <dariog_36@hotmail.com>
2025-03-08 18:17:03 -0500

    feat: boton de healt en el card muestra el board de health

--- src/app/animals/animals.component.ts
+++ src/app/animals/animals.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-animals',
+  standalone: true,
   imports: [],
   templateUrl: './animals.component.html',
   styleUrl: './animals.component.css'
 })
 export class AnimalsComponent {
-
+    @Output() selectHealth = new EventEmitter<string>();
+    showHealth(item:string){
+      this.selectHealth.emit(item);
+    }
 }
