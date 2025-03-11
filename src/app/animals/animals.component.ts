fef84451dabf3086fb20d4f196d41bafd59f9755
HernanGomezM <dariog_36@hotmail.com>
2025-03-10 20:15:35 -0500

    feat: boton para borrar la card seleccionada funcionando

--- src/app/animals/animals.component.ts
+++ src/app/animals/animals.component.ts
@@ -10,11 +10,18 @@ import { Component, EventEmitter, Output } from '@angular/core';
 export class AnimalsComponent {
     @Output() selectHealth = new EventEmitter<string>();
     countCards:number[] = [];
+    
 
     showHealth(item:string){
       this.selectHealth.emit(item);
     }
     plusCard(){
-      this.countCards.push(1);
+      const lastCard = this.countCards.length > 0 ? this.countCards.at(-1)! : 0;
+      this.countCards.push(lastCard+1);
+      console.log(`este es el ulltimo item: ${lastCard}`);
+    }
+
+    deleteCard($item:number, $index:number){
+      this.countCards.splice($index, $item);
     }
 }
