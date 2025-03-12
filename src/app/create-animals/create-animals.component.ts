a93901d3eece0d88da6016891b11014dc3baf11d
HernanGomezM <dariog_36@hotmail.com>
2025-03-12 14:50:41 -0500

    feat: creacion componente para crear animales

---
+++ src/app/create-animals/create-animals.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-animals',
+  imports: [],
+  templateUrl: './create-animals.component.html',
+  styleUrl: './create-animals.component.css'
+})
+export class CreateAnimalsComponent {
+
+}
