81fe9acb5ea07949e113858bb2d77f686dc2a2fc
HernanGomezM <dariog_36@hotmail.com>
2025-04-26 16:00:04 -0500

    feat: nuevo menu con la libreria primeng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,15 @@ import { LoginComponent } from "./login/login.component";
 import { FirebaseService } from './services/firebase.service';
 import { MessageService } from 'primeng/api';
 import { Toast } from 'primeng/toast';
+import { MenuItem } from 'primeng/api';
+import { Menu } from 'primeng/menu';
+import { ButtonModule } from 'primeng/button';
+
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MenuComponent, AnimalsComponent, HealthComponent, ReproducctionComponent, LoginComponent, Toast],
+  imports: [RouterOutlet, MenuComponent, AnimalsComponent, HealthComponent, ReproducctionComponent, LoginComponent, Toast, Menu, ButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   providers: [MessageService]
@@ -20,9 +24,11 @@ import { Toast } from 'primeng/toast';
 export class AppComponent {
     Board='';
     login = false;
-    constructor(private firabaseService: FirebaseService, private messageService: MessageService){
-
-      this.firabaseService.auth.onAuthStateChanged((user)=>{
+    itemsMenu: MenuItem[] | undefined;
+    user: any = null;
+    constructor(private firebaseService: FirebaseService, private messageService: MessageService){
+      
+      this.firebaseService.auth.onAuthStateChanged((user)=>{
         this.login = !!user;
         console.log(`Estado del login: ${this.login}`);
       });
@@ -35,6 +41,28 @@ export class AppComponent {
         detail: event.detail});
     }
 
+    ngOnInit(){
+      this.firebaseService.auth.onAuthStateChanged((user) => {
+        this.user = user;
+      });
+      this.itemsMenu = [{
+        label: this.user,
+        items:[
+          {
+            label: 'Configracion',
+            icon: 'fas fa-gear  '
+          },
+          {
+            label: 'Cerrar Sesion',
+            icon: 'fas fa-arrow-right-from-bracket',
+            command: ()=>{this.firebaseService.signOut()},
+          },
+        ]
+      }]
+    }
+    signOut(){
+      this.firebaseService.signOut();
+    }
     getBoard(boardName: string){
       this.Board = boardName;
     }
